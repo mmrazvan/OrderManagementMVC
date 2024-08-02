e9ef83ca2118f39c316a5cc7898d84bd0dcc40ee
mmrazvan <mmrazvan@yahoo.com>
2024-08-02 07:55:07 +0300

    Update .NET version, fix indentation, add dotnet-ef tool
    Updated HomeController.cs for consistent indentation.
    Changed OrderManagementMVC.csproj to target .NET 8.0.
    Updated package references to latest versions.
    Modified OrderManagementMVC.sln for new VS format.
    Added dotnet-tools.json to include dotnet-ef tool.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,26 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 
 using OrderManagementMVC.Models;
 
-namespace OrderManagementMVC.Controllers
-{
+namespace OrderManagementMVC.Controllers;
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+	public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+	public IActionResult Index()
         {
             return View();
         }        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+	public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-}
